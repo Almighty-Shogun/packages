5a9378dca75e595075d5b6c0580c83df3717dbdf
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-11-26 00:28:00 +0100

    Feature(prototype-extensions): Added more extensions to the number prototype

--- packages/prototype-extensions/src/number.ts
+++ packages/prototype-extensions/src/number.ts
@@ -1,8 +1,32 @@
 declare global {
     interface Number {
+        add(value: number): number;
+        subtract(value: number): number;
+        multiply(value: number): number;
+        divide(value: number): number;
+
         equals(value: number): boolean;
         isInRange(min: number, max: number): boolean;
+        isEven(): boolean
+        isLowerThan(value: number): boolean;
+        isGreaterThan(value: number): boolean;
     }
+}
+
+Number.prototype.add = function (this: number, value: number): number {
+    return this + value;
+}
+
+Number.prototype.subtract = function (this: number, value: number): number {
+    return this - value;
+}
+
+Number.prototype.multiply = function (this: number, value: number): number {
+    return this * value;
+}
+
+Number.prototype.divide = function (this: number, value: number): number {
+    return this / value;
 }
 
 Number.prototype.equals = function (this: number, value: number): boolean {
@@ -11,6 +35,18 @@ Number.prototype.equals = function (this: number, value: number): boolean {
 
 Number.prototype.isInRange = function (this: number, min: number, max: number): boolean {
     return this >= min && this <= max;
+}
+
+Number.prototype.isEven = function (this: number): boolean {
+    return this % 2 === 0;
+}
+
+Number.prototype.isLowerThan = function (this: number, value: number): boolean {
+    return this < value;
+}
+
+Number.prototype.isGreaterThan = function (this: number, value: number): boolean {
+    return this > value;
 }
 
 export {};
